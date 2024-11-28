f166ae9c0023ec61bae31b58f410f597eafe74e5
zer0divider <marxc9@outlook.de>
2024-11-28 09:37:36 +0100

    try check false

--- pipeline.py
+++ pipeline.py
@@ -12,7 +12,7 @@ pytest_result = Artifacts(["pyunit.xml"], when=When.always)
 
 # unit tests with pytest
 def run_pytest():
-    r = run(["python3", "-m", "pytes", "--junitxml", pytest_result.paths[0]], check=True)
+    r = run(["python3", "-m", "pytest", "--junitxml", pytest_result.paths[0]])
     return r.returncode
 
 jobs = JobStore()
