789879788677f4ca51f9cc4b5fc4cd9e76300cea
Cornelius Marx <marxc9@outlook.de>
2025-01-15 21:49:46 +0100

    y/z test jobs

--- pipeline.py
+++ pipeline.py
@@ -20,6 +20,9 @@ jobs = JobStore()
 
 jobs.unit_tests = Job("Unit Tests", JobConfig(stage=stages.test, work=run_pytest, artifacts=pytest_result))
 
+jobs.z = Job("Z", JobConfig(stage=stages.test))
+jobs.y = Job("Y", JobConfig(stage=stages.test))
+
 workflow = [
     Rule(variables.pipeline_source_is(PipelineSource.push) & variables.CI_OPEN_MERGE_REQUESTS.is_set(), when=When.never),
     Rule(when=When.always)
